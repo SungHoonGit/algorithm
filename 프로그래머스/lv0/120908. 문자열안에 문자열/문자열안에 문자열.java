1154803bcc37d93dbb63c8fbf0dd234aa4acd1e3
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-03-12 14:40:47 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+         answer = str1.indexOf(str2);
+         if (answer== -1) { answer = 2;}
+        else answer=1;
+        return answer;
+    }
+}
