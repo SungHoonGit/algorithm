fdfff9b35cb304bf4b4afd260bb9b9744d09e073
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-03-12 14:39:35 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n + 1];
+
+        for (int i = 2; i <= (int) Math.sqrt(n); ++i) {
+            if (arr[i] == 1) continue;
+            for (int j = i + i; j <= n; j += i) {
+                arr[j] = 1;
+            }
+        }
+        
+        for (int i = 2; i < arr.length; ++i) {
+            if (arr[i] != 1) ++answer;
+        }
+
+        return n-answer-1;
+    }
+}
