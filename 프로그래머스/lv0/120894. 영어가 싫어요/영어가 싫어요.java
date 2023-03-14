92c7ed7f261e721783245dc55f72992ae5d0eb3a
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-03-14 15:24:53 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.44 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        numbers = numbers.replaceAll("zero","0");
+        numbers = numbers.replaceAll("one","1");
+        numbers = numbers.replaceAll("two","2");
+        numbers = numbers.replaceAll("three","3");
+        numbers = numbers.replaceAll("four", "4");
+        numbers = numbers.replaceAll("five","5");
+        numbers = numbers.replaceAll("six","6");
+        numbers = numbers.replaceAll("seven","7");
+        numbers = numbers.replaceAll("eight","8");
+        numbers = numbers.replaceAll("nine","9");
+        answer = Long.parseLong(numbers); // Long으로 형변환
+        return answer;
+    }
+}
