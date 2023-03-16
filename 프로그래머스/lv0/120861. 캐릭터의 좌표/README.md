97837da83cbef3272e66a5fa621807c356eb3ac7
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-03-16 21:29:36 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.03 ms
+메모리: 78.6 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
-        int[] now = {0, 0};
-        
-        for (int i = 0; i < keyinput.length; i++){
-            if(keyinput[i].equals("left")) now[0] -= now[0]>-(board[0]/2)?1:0;
-            else if(keyinput[i].equals("right")) now[0] += now[0]<(board[0]/2)?1:0;
-            else if(keyinput[i].equals("down")) now[1] -= now[1]>-(board[1]/2)?1:0;
-            else if(keyinput[i].equals("up")) now[1] += now[1]<(board[1]/2)?1:0;
-        
-        }
-        return now;
+            int[] answer = {0,0};
+            int x = board[0]/2;
+            int y = board[1]/2;
+            for(String i : keyinput ) {
+            if ( i.equals( "left" ) && answer[0] > -x ) answer[0] --;
+            if ( i.equals("right") && answer[0] < x ) answer[0] ++; 
+            if ( i.equals("up") && answer [1] < y ) answer [1] ++;
+            if ( i.equals("down" ) && answer [1] > -y ) answer [1]--; // for end
+            }
+        return answer;
     }
 }
