f97cce29dfe35ef16b59383a8941e810bc555629
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-03-17 15:56:42 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -17,7 +17,8 @@ class Solution {
         String[] answer = new String[list.size()];
         for (int j = 0; j<list.size(); j++ ) {
             answer[j] = list.get(j);
-        }
+        } // for end
+        
         return answer;
     }
 }
