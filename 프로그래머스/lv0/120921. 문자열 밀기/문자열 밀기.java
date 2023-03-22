be419c182858b32bc58c422728a0c749dc8c25f7
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-03-22 14:04:20 +0900

    [level 0] Title: 문자열 밀기, Time: 4.15 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        String[] a = A.split("");
+        String[] b = B.split("");
+        
+	    Queue<String> queue = new LinkedList<>(); 	// String형 queue 선언, Linkedlist 이용 
+        Queue<String> queue2 = new LinkedList<>(); 	// String형 queue 선언, Linkedlist 이용 
+        for ( int i = 0; i<a.length; i++) {
+            queue.add(a[a.length-i-1]);     // queue에 값 1 추가
+            queue2.add(b[b.length-i-1]);     // queue에 값 1 추가
+        }// for end
+        
+        for(int i=0; i<queue.size(); i++) {
+            if (queue.equals(queue2)) {
+                System.out.print("queue"+queue);
+                System.out.print("queue2"+queue2);
+                System.out.println(answer);  
+                break;
+            } else {
+                queue.add(queue.peek());
+                queue.remove();
+                System.out.print("queue"+queue);
+                System.out.print("queue2"+queue2);                
+                System.out.println(answer);    
+                answer++;
+            }
+        }// for end
+
+        if ( answer ==queue.size()) {
+            answer = -1;
+        }
+           
+
+
+        System.out.print(queue);
+        System.out.print(b[0]);
+        
+        return answer;
+    }
+}
