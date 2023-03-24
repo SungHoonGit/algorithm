ee9258a3391b722501f03e683eb60e6b09b44a06
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-03-24 16:26:51 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.07 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int a = 0;
+        for(int i=0; i<seoul.length; i++) {
+            if( seoul[i].equals("Kim")) {
+                a = i ;
+            }
+        }
+        String answer = "김서방은 "+ a +"에 있다";
+        return answer;
+    }
+}
