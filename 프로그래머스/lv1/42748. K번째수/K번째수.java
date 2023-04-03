e96904c339f396229128175a836d495ffd513f63
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-04-03 14:35:20 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,28 +1,47 @@
-import java.util.Arrays;
-
 class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int n = 0;
+        int[] ret = new int[commands.length];
 
-    public int[] solution(int[] ar, int[][] commands) {
+        while(n < commands.length){
+            int m = commands[n][1] - commands[n][0] + 1;
 
-        int[] answer = new int[commands.length];
+            if(m == 1){
+                ret[n] = array[commands[n++][0]-1];
+                continue;
+            }
 
-        for (int i = 0; i < commands.length; i++) {
+            int[] a = new int[m];
+            int j = 0;
+            for(int i = commands[n][0]-1; i < commands[n][1]; i++)
+                a[j++] = array[i];
 
-            int ed = commands[i][1];
-            int st = commands[i][0];
+            sort(a, 0, m-1);
 
-            int[] temp = new int[ed - st + 1]; // 복사 배열 설정.
-            for (int j = 0; j <= ed-st; j++) {
-                temp[j] = ar[j + st - 1];
-            } // 배열 복사.
-
-            Arrays.sort(temp);
-
-            answer[i] = temp[commands[i][2]-1];
+            ret[n] = a[commands[n++][2]-1];
         }
 
-        return answer;
-
+        return ret;
     }
 
+    void sort(int[] a, int left, int right){
+        int pl = left;
+        int pr = right;
+        int x = a[(pl+pr)/2];
+
+        do{
+            while(a[pl] < x) pl++;
+            while(a[pr] > x) pr--;
+            if(pl <= pr){
+                int temp = a[pl];
+                a[pl] = a[pr];
+                a[pr] = temp;
+                pl++;
+                pr--;
+            }
+        }while(pl <= pr);
+
+        if(left < pr) sort(a, left, pr);
+        if(right > pl) sort(a, pl, right);
+    }
 }
