62e88bcb83f0ba17aeb5bd68783b98c0ef310d02
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-04-19 09:29:50 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.62 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        s = s.replaceAll("zero","0")
+             .replaceAll("one","1")
+             .replaceAll("two","2")
+             .replaceAll("three","3")
+             .replaceAll("four","4")
+             .replaceAll("five","5")
+             .replaceAll("six","6")
+             .replaceAll("seven","7")
+             .replaceAll("eight","8")
+             .replaceAll("nine","9");
+        System.out.print(s);
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
