ff20a3b84dfba306e528c9375e86af1634706e01
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-06-16 20:57:02 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 8.52 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,13 +1,10 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "0";
-
-        for (int i = food.length - 1; i > 0; i--) {
-            for (int j = 0; j < food[i] / 2; j++) {
-                answer = i + answer + i; 
-            }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            sb.append(Integer.toString(i).repeat(food[i] / 2));
         }
-
-        return answer;
+        return sb.toString() + "0" + sb.reverse().toString();
     }
+
 }
