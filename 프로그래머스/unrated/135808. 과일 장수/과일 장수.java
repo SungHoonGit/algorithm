9e93b5a1687d0bbfe761cbcbd35a9f05a300f90c
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-06-16 21:17:22 +0900

    [unrated] Title: 과일 장수, Time: 0.52 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -5,9 +5,14 @@ class Solution {
         int answer = 0;
         Arrays.sort(score);
         // System.out.println(Arrays.toString(score));
-            for(int i = score.length-1;  i>=m-1; i-=m) {
-                answer += score[i-m+1]*m;
+            // for(int i = score.length-1;  i>=m-1; i-=m) {
+            //     answer += score[i-m+1]*m;
+            // }
+        
+            for(int i = score.length; i >= m; i -= m){
+                answer += score[i - m] * m;
             }
+
         return answer;
     }
 }
