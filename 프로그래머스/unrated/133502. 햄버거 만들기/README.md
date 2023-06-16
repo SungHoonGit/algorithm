03e7742b9fc1d84c1072ebc7472735dff74e82f0
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-06-16 20:39:52 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/README.md
+++ 프로그래머스/unrated/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 1.48 ms
+메모리: 72.8 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,36 +1,15 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-
-        ArrayList<Integer> ingredientList = new ArrayList<> (
-            Arrays.stream(ingredient).boxed().collect(Collectors.toList())  );
-
-        int lastBbangIndex = 0;
-
-        for (int i = 0 ; i < ingredientList.size() && ingredientList.size() > 3 ; i++ ) {
-            if (ingredientList.get(i) == 1) {
-                if (i > 2) {
-                    if (ingredientList.get(i-1) == 3 && 
-                        ingredientList.get(i-2) == 2 &&
-                        ingredientList.get(i-3) == 1) {
-                        ingredientList.remove(i);
-                        ingredientList.remove(i-1);
-                        ingredientList.remove(i-2);
-                        ingredientList.remove(i-3);
-
-                        answer++;
-
-                        i -= 4; // 4칸 전으로 간다. 다음 루프 때 for 증감문(i++)으로 인해 1칸 뒤로 간다.
-
-                        continue;
-                    }
+        for(int i=0; i<ingredient.length-3; i++){
+            if(ingredient[i]==0) continue;
+            if(ingredient[i]==1&&ingredient[i+1]==2&&ingredient[i+2]==3&&ingredient[i+3]==1){
+                for(int j=i+3; j>0;j--){
+                    ingredient[j] = (j>3) ? ingredient[j-4] : 0;    
                 }
+                answer++;    
             }
-        }
-
+        }      
         return answer;
     }
 }
