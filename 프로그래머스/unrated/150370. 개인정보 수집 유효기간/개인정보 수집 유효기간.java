a2fd8618473f237a7c8b8f4f6a29ea81c15fa93d
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-06-26 16:46:39 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 3.56 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,75 +1,35 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
-    
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answerList = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> termMap = new HashMap<>();
+        int date = getDate(today);
 
+        for (String s : terms) {
+            String[] term = s.split(" ");
+
+            termMap.put(term[0], Integer.parseInt(term[1]));
+        }
         for (int i = 0; i < privacies.length; i++) {
-            String[] privacyTokens = privacies[i].split(" ");
-            String targetDate = privacyTokens[0];
-            String targetItem = privacyTokens[1];
+            String[] privacy = privacies[i].split(" ");
 
-            for (String term : terms) {
-                String[] termTokens = term.split(" ");
-                String termItem = termTokens[0];
-                String termOffset = termTokens[1];
-
-                if (targetItem.equals(termItem)) {
-                    String expiryDate = getExpiryDate(targetDate, termOffset);
-                    if (isExpired(expiryDate, today)) {
-                        answerList.add(i + 1);
-                        break;
-                    }
-                }
+            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
+                answer.add(i + 1);
             }
         }
-        
-  
-        System.out.print(answerList);
-        int[] answer = new int[answerList.size()];
-        for (int k = 0; k < answerList.size(); k++) {
-            answer[k] = answerList.get(k);
-        }   
-        
-        return answer;
+        return answer.stream().mapToInt(integer -> integer).toArray();
     }
-    
-    
-    public String getExpiryDate(String startDate, String offset) {
-        String[] startDateTokens = startDate.split("\\.");
-        int year = Integer.parseInt(startDateTokens[0]);
-        int month = Integer.parseInt(startDateTokens[1]);
-        int day = Integer.parseInt(startDateTokens[2]);
 
-        int offsetMonth = Integer.parseInt(offset);
-        int offsetYear = offsetMonth / 12;
-        offsetMonth %= 12;
-        
-        year += offsetYear;
-        month += offsetMonth;
-        if (month > 12) {
-            month -= 12;
-            year++;
-        }
-
-        return String.format("%d.%02d.%02d", year, month, day);
-
-    }
-    
-    public boolean isExpired (String privacies, String today) {
-        String[] changedate = privacies.split("\\.");
-        String[] todays = today.split("\\.");
-
-        int yearComparison = changedate[0].compareTo(todays[0]);
-        int monthComparison = changedate[1].compareTo(todays[1]);
-        int dayComparison = changedate[2].compareTo(todays[2]);
-
-        if (yearComparison > 0 || (yearComparison == 0 && monthComparison > 0) ||
-            (yearComparison == 0 && monthComparison == 0 && dayComparison > 0)) {
-            return false; // 유통기한이 지남
-        }
-
-        return true; // 유통기한이 지나지 않음
-        
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
