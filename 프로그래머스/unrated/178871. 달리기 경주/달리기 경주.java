aac56f44ae1ae63e9bdfd67edfaa9ef28f6ea000
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-06-28 22:03:39 +0900

    [unrated] Title: 달리기 경주, Time: 0.27 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -1,36 +1,64 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        Map<String, Integer> playerRanks = new HashMap<>(); // 각 선수의 등수를 저장하는 Map
-        List<String> playerList = new ArrayList<>(); // 선수들의 순서를 저장하는 List
 
-        // 초기 등수 설정 및 선수 리스트 초기화
-        for (int i = 0; i < players.length; i++) {
-            String player = players[i];
-            playerRanks.put(player, i + 1); // 선수의 초기 등수 설정
-            playerList.add(player); // 선수 리스트에 추가
+        RacingNode root = new RacingNode(), node = root;
+        HashMap<String, RacingNode> map = new HashMap<>();
+
+        for (String player : players) {
+            node.next = new RacingNode(player, node);
+            node = node.next;
+            map.put(player, node);
         }
 
-        // 해설진의 부른 이름에 따라 등수 변경
-        for (String calling : callings) {
-            int index = playerRanks.get(calling) - 1; // 부른 이름의 인덱스 (등수에 맞는 인덱스로 변환)
-            if (index > 0) {
-                String temp = playerList.get(index - 1); // 앞에 있는 선수의 이름
-                playerList.set(index - 1, calling); // 부른 이름과 앞에 있는 선수의 순서 변경
-                playerList.set(index, temp);
-                playerRanks.put(calling, playerRanks.get(temp)); // 등수 Map 업데이트
-                playerRanks.put(temp, playerRanks.get(temp) + 1);
-            }
+        RacingNode[] arr = new RacingNode[4];
+
+        for (String call : callings) {
+            RacingNode runner = map.get(call);
+            arr[0] = runner.prev.prev;
+            arr[1] = runner;
+            arr[2] = runner.prev;
+            arr[3] = runner.next;
+
+            arr[0].next = arr[1];
+            arr[1].next = arr[2];
+            arr[2].next = arr[3];
+
+            arr[1].prev = arr[0];
+            arr[2].prev = arr[1];
+            if (arr[3] != null)
+                arr[3].prev = arr[2];
+
         }
 
-        String[] result = new String[playerList.size()];
-        for (int i = 0; i < playerList.size(); i++) {
-            result[i] = playerList.get(i);
+        int idx = 0;
+        root = root.next;
+
+        while (root != null) {
+            players[idx++] = root.name;
+            root = root.next;
         }
-        return result;
+
+        return players;
     }
+}
+
+class RacingNode {
+
+    RacingNode prev, next;
+    String name;
+
+    RacingNode() {
+    }
+
+    RacingNode(String name) {
+        this.name = name;
+    }
+
+    RacingNode(String name, RacingNode prev) {
+        this(name);
+        this.prev = prev;
+    }
+
 }
