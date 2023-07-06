adeed189c43a3c0bf228f6a1f3547dbcbda2adbb
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-07-06 15:29:10 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,22 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int count = countOnes(n);
-        int m = n + 1;
-
-        while (true) {
-            if (countOnes(m) == count) {
-                return m;
-            }
-            m++;
-        }
-    }
-    
-    private int countOnes(int num) {
-        int count = 0;
-        while (num != 0) {
-            count += num & 1;
-            num = num >>> 1;
-        }
-        return count;
+         int a = Integer.bitCount(n);
+          while (Integer.bitCount(++n) != a) {}
+          return n;
     }
 }
