0bf1463fc562355d9e4b10198209c417d53f8bcb
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-07-09 23:16:53 +0900

    Update N개의 최소공배수.java
    함수로도 풀어보기

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,16 +1,30 @@
-import java.math.BigInteger;
+// import java.math.BigInteger;
 
 class Solution {
     public int solution(int[] arr) {
         int answer = arr[0];
-        BigInteger gcd;
-        BigInteger lcm;
+        // BigInteger gcd;
+        // BigInteger lcm;
         for(int i=0; i< arr.length-1; i++) {
-         gcd = BigInteger.valueOf(arr[i+1]).gcd(BigInteger.valueOf(answer));  // 최대공약수 계산
-         lcm = BigInteger.valueOf(arr[i+1]).multiply(BigInteger.valueOf(answer)).divide(gcd);  // 최소공배수 계산            
-            answer=lcm.intValue();
+         // gcd = BigInteger.valueOf(arr[i+1]).gcd(BigInteger.valueOf(answer));  // 최대공약수 계산
+         // lcm = BigInteger.valueOf(arr[i+1]).multiply(BigInteger.valueOf(answer)).divide(gcd);  // 최소공배수 계산            
+         //   answer=lcm.intValue();
+         answer = lcm(answer, arr[i+1]);
             
         }
         return answer;
     }
+    
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    } // 최대공약수
+    
+    public int lcm(int a, int b) {
+        int gcdValue = gcd(a, b);
+        return (a * b) / gcdValue;
+    } // 최소공배수
+    
 }
