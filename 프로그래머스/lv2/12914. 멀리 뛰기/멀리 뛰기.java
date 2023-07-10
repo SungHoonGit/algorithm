470170a2b673e4f634233a8719c01a6ac4bd50c9
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-07-10 13:37:18 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+
+    public long solution(int n) {
+        long answer = 0;
+        if (n==1) {
+          return 1;  
+        }  else if (n==2) {
+          return 2;  
+        } 
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2;i<n;i++)  {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        answer = dp[n-1];
+        return answer;
+    }
+
+}
