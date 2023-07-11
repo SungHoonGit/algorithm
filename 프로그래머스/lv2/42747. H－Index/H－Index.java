9854c7a47a35bb6e89f2cab901ef54f352311f30
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-07-11 17:15:01 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,16 +1,21 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] citations) {
+        PriorityQueue<Integer> citationQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < citations.length; i++) {
+            citationQ.add(citations[i]);
+        }
+
         int answer = 0;
 
-        Arrays.sort(citations);
-        for(int i=0; i<citations.length; i++){
-            if(citations[i]>=citations.length-i){
-                answer=citations.length-i;
-                break;
-            }
+        while (!citationQ.isEmpty() && answer < citationQ.peek()) {
+            citationQ.poll();
+            answer++;
         }
+
         return answer;
     }
 }
