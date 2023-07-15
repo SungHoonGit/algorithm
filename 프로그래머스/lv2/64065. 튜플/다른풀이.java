8fb0c92dde8021e987702ba8d471ec67a2b83dc0
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-07-15 20:10:17 +0900

    Create 다른풀이.java
    Pattern /. Match 사용한 풀이

---
+++ 프로그래머스/lv2/64065. 튜플/다른풀이.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class Solution {
+    public int[] solution(String s) {
+
+        Map<String, Integer> map = new HashMap<>();
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            String n = matcher.group();
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        int size = map.size();
+        int[] answer = new int[size];
+        for (String key: map.keySet()) {
+            answer[size - map.get(key)] = Integer.parseInt(key);
+        }
+        return answer;
+    }
+
+}
+
+// 다른 풀이
