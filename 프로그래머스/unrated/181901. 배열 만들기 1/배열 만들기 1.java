1a390c6d58f429e3c677f859b95fece47f107b6b
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-09-26 00:59:46 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        for(int i=0; i<n/k; i++) {
+            answer[i] = k+i*k;
+        }
+        return answer;
+    }
+}
