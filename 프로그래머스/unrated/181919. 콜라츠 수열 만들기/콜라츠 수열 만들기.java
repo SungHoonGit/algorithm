fe01b20805209d1b87ee4cc9b230f63f48998fe1
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-10-09 00:01:36 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -3,27 +3,22 @@ import java.util.List;
 
 class Solution {
     public int[] solution(int n) {
-        // 콜라츠 수열을 저장할 리스트 생성
         List<Integer> collatzSequence = new ArrayList<>();
-        collatzSequence.add(n); // 초기값 n을 리스트에 추가
-
+        collatzSequence.add(n);
         while (n != 1) {
             if (n % 2 == 0) {
-                // 짝수인 경우, 2로 나누기
                 n /= 2;
             } else {
-                // 홀수인 경우, 3을 곱하고 1을 더하기
                 n = 3 * n + 1;
             }
-            collatzSequence.add(n); // 새로운 값을 리스트에 추가
+            collatzSequence.add(n);
         }
 
-        // 리스트를 배열로 변환하여 반환
         int[] result = new int[collatzSequence.size()];
         for (int i = 0; i < collatzSequence.size(); i++) {
             result[i] = collatzSequence.get(i);
         }
-        
-        return result;            
+
+        return result;
     }
 }
