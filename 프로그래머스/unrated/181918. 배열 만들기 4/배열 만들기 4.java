29026f09f90c14cf617a3ff3926d527d75fa64d7
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-10-09 23:49:53 +0900

    [level unrated] Title: 배열 만들기 4, Time: 6.18 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+
+        while (i < arr.length) {
+            if (stk.isEmpty() || stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) >= arr[i]) {
+                stk.remove(stk.size() - 1);
+            }
+        }
+
+        int[] result = new int[stk.size()];
+        for (int j = 0; j < stk.size(); j++) {
+            result[j] = stk.get(j);
+        }
+
+        return result;
+    }
+}
