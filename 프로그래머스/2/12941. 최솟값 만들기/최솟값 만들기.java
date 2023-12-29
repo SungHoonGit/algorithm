7d088fbf8709ea0119680e714b537b70501ce61f
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-12-29 14:58:29 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.12 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution
+{    
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        PriorityQueue<Integer> b = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i=0; i<A.length; i++) {
+            a.add(A[i]);
+            b.add(B[i]);
+        }
+
+        while (!a.isEmpty()) {
+            answer += a.remove()*b.remove();
+        }
+
+        return answer;
+    }
+}
