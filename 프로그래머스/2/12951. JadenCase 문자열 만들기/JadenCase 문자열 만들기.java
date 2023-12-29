efaa433f5c6f12fa5071af5a1d6accd3183c80a2
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2023-12-29 14:20:08 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.29 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,3 +1,5 @@
+import java.util.regex.*;
+
 class Solution {
     public String solution(String s) {
         
@@ -10,5 +12,16 @@ class Solution {
             isTrue = i.equals(" ") ? true : false;
         }
         return answer;
+    }
+}
+
+
+class Solution2 {
+    public String solution(String s) {
+        Matcher m = Pattern.compile("\\b([\\w])([\\w]*)").matcher(s);
+        while (m.find()) {
+            s = s.replaceAll("\\b" + m.group(), m.group(1).toUpperCase() + m.group(2).toLowerCase());
+        }
+        return s;
     }
 }
