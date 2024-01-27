747fe6ae09d62146fe7c2f9319f8fc1ddce1f180
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2024-01-27 13:47:16 +0900

    [level 0] Title: 배열 조각하기, Time: 8.73 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 class Solution {
-    public int[] solution(int[] arr, int[] query) {
+    public int[] solution2(int[] arr, int[] query) {
         for(int i =0; i<query.length; i++) {
             if(i % 2 == 0) {
                 arr = Arrays.copyOfRange(arr, 0, query[i]+1);
@@ -11,4 +11,18 @@ class Solution {
         }
         return arr;
     }
+    
+   public int[] solution(int[] arr, int[] query) {
+        int start = 0;
+        int end = arr.length - 1;
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                end = start + query[i] - 1;
+            } else {
+                start += query[i];
+            }
+        }
+        System.out.print(start + ", " +end);
+        return Arrays.copyOfRange(arr, start, end + 2);
+    }    
 }
