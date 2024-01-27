1f363cd86df9eb1108d99c90406a98d0b55a3635
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2024-01-27 14:17:32 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+
+        int count=0;
+        boolean check = true;
+        
+        for(String str : str_list) {
+            if(str.equals("l") ){
+                check = true;
+                break;
+            } else if(str.equals("r")) {
+                check = false;
+                break;
+            }
+            count++;
+        }
+        if(count == str_list.length) count =0;
+        if(check) {
+            str_list = Arrays.copyOfRange(str_list, 0, count);
+        }else {
+            str_list = Arrays.copyOfRange(str_list, count+1, str_list.length);
+        }
+        
+        return str_list;
+    }
+}
