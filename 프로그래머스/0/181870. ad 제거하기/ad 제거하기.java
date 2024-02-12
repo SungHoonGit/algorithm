3cc649663984cf5f6c0d22e83f5d4ef9b7627676
SungHoon kim <110158529+SungHoonGit@users.noreply.github.com>
2024-02-12 23:55:30 +0900

    [level 0] Title: ad 제거하기, Time: 1.52 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -4,12 +4,11 @@ class Solution {
     public String[] solution(String[] strArr) {
         
         ArrayList<String> StringArray = new ArrayList<String>(); // 타입 지정
-        int count = 0;
+        
         for(String str : strArr) {
             if(!str.contains("ad")) {
                 StringArray.add(str);
-            }
-            count++;
+            }    
         }
         System.out.print(StringArray);
         // String[] answer = StringArray.toArray(new String[StringArray.size()]);
